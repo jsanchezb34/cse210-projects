ba62f11d94cf2d27e839c81916131220320edcf2
jsanchezb34 <jsanchezb34@>
2025-09-05 19:58:49 -0400

    week 1 excercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your name? .");
+        string name = Console.ReadLine();
+        Console.WriteLine("What is your last name? .");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}");
     }
 }
