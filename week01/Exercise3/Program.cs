3d227889dc022a37e38eec5118387c8c41030dcd
jsanchezb34 <jsanchezb34@>
2025-09-06 17:42:25 -0400

    week 1 exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magic = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magic)
+        {
+            if (guess > magic)
+            {
+                Console.WriteLine("lower");
+            }
+
+            else if (guess < magic)
+            {
+                Console.WriteLine("higher");
+            }
+
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+            
+       Console.WriteLine("You guessed it!");
+
     }
 }
