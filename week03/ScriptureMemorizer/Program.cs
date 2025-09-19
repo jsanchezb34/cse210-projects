fd2cef587a254c3698215ee9a3d78333263fbd3e
jsanchezb34 <jsanchezb34@>
2025-09-19 18:45:52 -0400

    Scripture update functions added

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,21 +6,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.\n");
-        
+
         Reference ref1 = new Reference("1 Nephi", 3, 7);
         Scripture scripture1 = new Scripture(ref1, "And it came to pass that I, Nephi, said unto my father: \nI will go and do the things which the Lord hath commanded, \nfor I know that the Lord giveth no commandments unto the children of men, \nsave he shall prepare a way for them that they may accomplish \nthe thing which he commandeth them.");
-        
+
         // This will start by displaying "AAA" and waiting for the user to press the enter key
         Console.WriteLine(scripture1.GetDisplayText());
         Console.WriteLine("\nPress enter to continue or type 'quit' to exit.");
 
         Console.ReadLine();
 
-        // This will clear the console
-        Console.Clear();
+        while (true)
+        {
+            // This will clear the console
+            Console.Clear();
+            Console.WriteLine(scripture1.GetDisplayText());
+            Console.WriteLine("\nPress enter to continue or type 'quit' to exit.");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "quit")
+                break;
 
-        // This will show "BBB" in the console where "AAA" used to be
-        Console.WriteLine("BBB");
-        
+            scripture1.HideRandomWord();
+            // This will show "BBB" in the console where "AAA" used to be
+            Console.WriteLine("BBB");
+
+        }
     }
 }
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -13,10 +13,26 @@ public class Scripture
             .ToList();
     }
     public string GetDisplayText()
-    { 
-        string joinedWords = string.Join (" ", _words.Select(word => word.GetDisplayText()));
+    {
+        string joinedWords = string.Join(" ", _words.Select(word => word.GetDisplayText()));
         return $"{_reference.GetDisplayText()} {joinedWords}";
 
+    }
+
+    public void HideRandomWord()
+    {
+        var visibleWords = _words.Where(w => w.GetDisplayText() != "_____").ToList();
+        if (visibleWords.Count > 0)
+        {
+            Random rand = new Random();
+            int index = rand.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+        }
+    }
+
+    public bool AllWordsHidden()
+    {
+        return _words.All(w=> w.GetDisplayText() == "_____");
     }
 
     
