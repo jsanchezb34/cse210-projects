e8abcb1a2aea4d457cff911bbb627005901ddab1
jsanchezb34 <jsanchezb34@>
2025-10-03 12:14:15 -0400

    Week5 Project 1st update

---
+++ week05/Mindfulness/Breathing.cs
@@ -0,0 +1,20 @@
+
+public class Breathing
+{
+    public void Inhale()
+    {
+        System.Console.WriteLine("Inhaling...");
+        Thread.Sleep(1000);
+    }
+
+
+
+    public void Exhale()
+    {
+        // Code to simulate exhaling
+        System.Console.WriteLine("Exhaling...");
+        Thread.Sleep(1000);
+    }
+
+    
+}
