76ff3334410724037277dfca34341d19e5cbbb15
jsanchezb34 <jsanchezb34@>
2025-10-07 10:45:03 -0400

    week5 project

--- week05/Mindfulness/Reflecting.cs
+++ week05/Mindfulness/Reflecting.cs
@@ -1,5 +1,21 @@
-
-public class ReflectingActivity()
+public class Breathing : activity
 {
-    
+    public Breathing() : base("breathing",
+    "This activity will help you relax by walking you through \nbreathing in and out slowly.")
+    {
+    }
+    public void Run()
+    {
+        Start();
+        int elapsed = 0;
+        while (elapsed < _duration)
+        {
+            Console.Write("inhaling");
+            Showspinnner(5);
+            Console.Write("Exhaling");
+            Showspinnner(5);
+            elapsed += 10;
+        }
+        End();
+    }
 }
