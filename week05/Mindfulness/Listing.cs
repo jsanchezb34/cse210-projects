2372f02814e7d282dce015a045acd879dae8aa13
jsanchezb34 <jsanchezb34@>
2025-10-08 11:04:11 -0400

    Week5 project

--- week05/Mindfulness/Listing.cs
+++ week05/Mindfulness/Listing.cs
@@ -1,16 +1,50 @@
 
-public class listing: activity
+using System.Diagnostics;
+
+public class Listing: activity
 {
-    public listing() : base("Listing",
+    public Listing() : base("Listing",
     "\nThis activity will help you reflect on the good things in your life\nby having you list as many things as you can in a certain area.")
     {
     }
-    
+
+    private string[] _prompts ={
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    private string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        return _prompts[rand.Next(_prompts.Length)];
+    }
     
     public void Run()
     {
         Start();
+        Console.WriteLine("\nList as many responses as you can to the following prompt:");
+        Console.WriteLine("\nType **Stop** to finish");
+        Console.WriteLine($"\n{GetRandomPrompt()}\n");
+        Console.WriteLine("Beggin in: ");
+        ShowCountdown(5);
 
+        List<string> responses = new List<string>();
+        Stopwatch timer = new Stopwatch();
+        timer.Start();
+
+        while (timer.Elapsed.TotalSeconds < GetDuration())
+        {
+            Console.Write("> ");
+            string response = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(response))
+            {
+                responses.Add(response);
+            }
+        }
+        Console.WriteLine($"\nYou listed {responses.Count} items!");
         End();
     }
 }
--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,20 +5,54 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.\n");
-        TestBreathingActivity();
-        TestReflectingActivity();
 
-    }
+        bool running = true;
 
-    static void TestBreathingActivity()
-    {
-        Breathing breathingActivity = new Breathing();
-        breathingActivity.Run();
-    }
 
-    static void TestReflectingActivity()
-    {
-        Reflecting reflectingActivity = new Reflecting();
-        reflectingActivity.Run();
+        while (running)
+        {
+            Console.WriteLine("What activity would you like to try");
+            Console.WriteLine("1. Breathing");
+            Console.WriteLine("2. Reflecting");
+            Console.WriteLine("3. Listing");
+            Console.WriteLine("4. Quit");
+            Console.WriteLine("Enter the number (1-4)");
+            string choice = Console.ReadLine();
+
+            Console.Clear();
+            if (choice == "1")
+            {
+                Breathing breathingActivity = new Breathing();
+                breathingActivity.Run();
+            }
+
+            else if (choice == "2")
+            {
+                Reflecting reflectingActivity = new Reflecting();
+                reflectingActivity.Run();
+            }
+
+            else if (choice == "3")
+            {
+                Listing ListingActivity = new Listing();
+                ListingActivity.Run();
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("\nThank you for using the Mindfulness Project. Goodbye!");
+                running = false;
+            }
+            else
+            {
+                Console.WriteLine("Please select a valid option (a number between 1 and 4)\n");
+            }
+
+            if (running)
+            {
+                Console.WriteLine("\nPress Enter to return to the menu...");
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
     }
 }
