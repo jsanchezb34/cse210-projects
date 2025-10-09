741628d45cfd96e028571c89aad39983a0850e98
jsanchezb34 <jsanchezb34@>
2025-10-09 15:33:59 -0400

    Week6 learning activityprogram update

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        Shape square = new Square("Red", 3);
+        Shape rectangle = new Rectangle("bue", 4, 5);
+        Shape circle = new Circle("Green", 6);
+
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}");
+            Console.WriteLine($"Shape Area: {shape.GetArea()}");
+            Console.WriteLine($"--------------------------------");
+        }
     }
 }
